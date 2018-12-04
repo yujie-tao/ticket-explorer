fd98ae3a6f07bbaf5bf5972ee05a995368322815
HanGuo97 <alex.guohan1106@gmail.com>
2018-12-03 22:04:35 -0500

    supported get/create/createFake for almost all the resource types; implemented sortFlightsByDepartTime and sortTicketsByPrice

--- utils/utils.js
+++ utils/utils.js
@@ -124,25 +124,57 @@ function _DefaultErrorFn(tag) {
  * ----------------------------------------------------------------
  */
 
+// API's: Just use them
+function sortFlightsByDepartTime(){
+  getFlights(_SortByDepartTime);
+}
+
+function sortTicketsByPricePaid(){
+  getTickets(_SortByPricePaid)
+}
+
+
+
+// Functions ------------------------------------------------------
+function _PrintResponse(response) {
+  console.log(response);
+}
+
+function _SortByDepartTime(response) {
+  // https://stackoverflow.com/questions/10123953/sort-javascript-object-array-by-date/10124053
+  response.sort(function(r1, r2) {
+    // Turn your strings into dates, and then subtract them
+    // to get a value that is either negative, positive, or zero.
+    return new Date(r1.departs_at) - new Date(r2.departs_at);
+  });
+  console.log(response);
+}
+
+function _SortByPricePaid(response){
+  response.sort(function(r1, r2) {
+    return r1.price_paid - r2.price_paid;
+  });
+
+  console.log(response);
+}
+
+
 // Get ------------------------------------------------------------
-function getFlights() {
+// https://stackoverflow.com/questions/5316697/jquery-return-data-after-ajax-call-success
+function getFlights(response_fn = _PrintResponse) {
   AjaxUtils.index(
     "flights",
-    function(response) {
-      console.log(response)
-    },
+    response_fn,
     function(response) {
       alert("getFlights Failed");
     }
   )
 }
 
-function getAirlines() {
+function getAirlines(response_fn = _PrintResponse) {
   AjaxUtils.index(
     "airlines",
-    function(response) {
-      console.log(response)
-    },
+    response_fn,
     function(response) {
       alert("getAirlines Failed");
     }
@@ -150,12 +182,10 @@ function getAirlines() {
 }
 
 
-function getAirports() {
+function getAirports(response_fn = _PrintResponse) {
   AjaxUtils.index(
     "airports",
-    function(response) {
-      console.log(response)
-    },
+    response_fn,
     function(response) {
       alert("getAirports Failed");
     }
@@ -163,14 +193,56 @@ function getAirports() {
 }
 
 
-function getPlanes() {
+function getPlanes(response_fn = _PrintResponse) {
   AjaxUtils.index(
     "planes",
-    function(response) {
-      console.log(response)
-    },
+    response_fn,
     function(response) {
       alert("getPlanes Failed");
+    }
+  )
+}
+
+
+function getSeats(response_fn = _PrintResponse) {
+  AjaxUtils.index(
+    "seats",
+    response_fn,
+    function(response) {
+      alert("getSeats Failed");
+    }
+  )
+}
+
+
+function getInstances(response_fn = _PrintResponse) {
+  AjaxUtils.index(
+    "instances",
+    response_fn,
+    function(response) {
+      alert("getInstances Failed");
+    }
+  )
+}
+
+
+function getItineraries(response_fn = _PrintResponse) {
+  AjaxUtils.index(
+    "itineraries",
+    response_fn,
+    function(response) {
+      alert("getItineraries Failed");
+    }
+  )
+}
+
+
+function getTickets(response_fn = _PrintResponse) {
+  AjaxUtils.index(
+    "tickets",
+    response_fn,
+    function(response) {
+      alert("getTickets Failed");
     }
   )
 }
@@ -222,8 +294,59 @@ function createPlane(name) {
 }
 
 
+function createSeat(plane_id, row, number) {
+  AjaxUtils.create(
+    "seats", {
+      seat: {
+        plane_id: plane_id,
+        row: row,
+        number: number,
+      }
+    });
+}
+
+
+function createInstance(flight_id, date) {
+  AjaxUtils.create(
+    "instances", {
+      instance: {
+        flight_id: flight_id,
+        date: date
+      }
+    });
+}
+
+
+function createItinerary(confirmation_code) {
+  AjaxUtils.create(
+    "itineraries", {
+      itinerary: {
+        confirmation_code: confirmation_code,
+        email: "kmp@cs.unc.edu"
+      }
+    });
+}
+
+
+function createTicket(first_name, last_name, age, gender, price_paid, instance_id, seat_id) {
+  AjaxUtils.create(
+    "tickets", {
+      ticket: {
+        first_name: first_name,
+        last_name: last_name,
+        age: age,
+        gender: gender,
+        // is_purchased: true,
+        price_paid: price_paid,
+        instance_id: instance_id,
+        seat_id: seat_id
+      }
+    });
+}
+
+
 // FakeData -------------------------------------------------------
-function createFakeFlights(){
+function createFakeFlights() {
   createFlight("4:30", "7:10", "AA 267", 129406, 129407);
   createFlight("5:30", "7:10", "AA 267", 129406, 129408);
   createFlight("6:30", "7:10", "AA 667", 129407, 129409);
@@ -255,6 +378,43 @@ function createFakePlanes() {
   createPlane("Plane-3");
   createPlane("Plane-4");
   createPlane("Plane-5");
+}
+
+function createFakeSeats() {
+  createSeat(4506, 1, "A");
+  createSeat(4510, 2, "B");
+  createSeat(4507, 1, "C");
+  createSeat(4509, 2, "A");
+  createSeat(4506, 1, "B");
+  createSeat(4508, 2, "C");
+  createSeat(4510, 1, "A");
+  createSeat(4510, 2, "B");
+  createSeat(4506, 1, "C");
+  createSeat(4510, 2, "D");
+}
+
+function createFakeInstances() {
+  createInstance(139520, "2018-12-21");
+  createInstance(139521, "2018-12-21");
+  createInstance(139522, "2018-12-21");
+  createInstance(139523, "2018-12-21");
+  createInstance(139234, "2018-12-21");
+}
+
+function createFakeItineraries() {
+  createItinerary("Itinerary-1");
+  createItinerary("Itinerary-2");
+  createItinerary("Itinerary-3");
+  createItinerary("Itinerary-4");
+  createItinerary("Itinerary-5");
+}
+
+function createFakeTickets(){
+  createTicket("Yujie-1", "Tao", "20", "Female", "119.99", 874854, 13157);
+  createTicket("Yujie-2", "Tao", "20", "Female", "999.0", 874855, 13159);
+  createTicket("Yujie-3", "Tao", "20", "Female", "23.7", 874856, 13161);
+  createTicket("Yujie-4", "Tao", "20", "Female", "432.1", 874857, 13163);
+  createTicket("Yujie-5", "Tao", "20", "Female", "77.3", 874858, 13165);
 }
 
 
