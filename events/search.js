ef7be9e109d96997740c341a2fcd5a4cab644a76
yujie-tao <yujietao@outlook.com>
2018-12-08 23:15:35 -0500

    for refernce, for fun

--- events/search.js
+++ events/search.js
@@ -1,14 +1,55 @@
-var depCode = 'LAX';
+var depCode = 'BOS';
 var arrCode = 'JFK';
-var depdate = '2000-01-01';
+var depDate = '2018-11-29';
+
+// searchFlight(depCode, arrCode, depDate);
+
+function searchFlight(depCode,arrCode, depDate){
+	getAirports(function(response){
+					if(response.length == 0){
+						/******handle not found in front end*****/
+						return;
+					}
+					let depId = response[0].id;
+					getAirports(function(response){
+						if(response.length == 0){
+							/******handle not found in front end*****/
+							return;
+						}
+						let arrId = response[0].id;
+						getFlights(function(response){
+							if(response.length == 0){
+								/******handle not found in front end*****/
+								return;
+							}
+							for(var i = 0; i < response.length;i++){
+								console.log(response[i]);
+								let fid = response[0].id;
+								/******need to be splitted by T*****/
+								let arrTime = response[0].arrives_at;
+								let depTime = response[0].departs_at;
+								getInstances(function(response){
+									if(response.length == 0){
+										/******handle not found in front end*****/
+										// return
+									}
+									console.log(response);
+									let iid = response[0].id;
+									getTickets(function(){
+										console.log(response);
+									},{instance_id:iid});
+								},{flight_id:fid, date:depDate});
+							}
+						},{departure_id:depId,arrival_id:arrId});
+					},{code:arrCode});
+				},{code: depCode});	
+}
 
 var x;
-getAirports(function(response){
-			  let depId = response[0].id;
-			  getAirports(function(response){
-			  	let arrId = response[0].id;
-			  	getFlights(function(response){
-			  		console.log(response)
-			  	},{departure_id:depId,arrival_id:arrId});
-			  },{code:arrCode});
-			},{code: depCode});
+
+getAirports(function(response){x=response},{code:depCode});
+// console.log(x);
+
+setTimeout(function(){
+  console.log(x);
+},2000);
